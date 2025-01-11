fd09311a5d2b1d04c424a85d21d2214c0ee2c9f8
javiscripto <javier.mecker94@gmail.com>
2025-01-10 22:53:32 -0300

    implementacion de fetching de notas utilizando el estado global

--- navigation/mainNavigation.jsx
+++ navigation/mainNavigation.jsx
@@ -1,9 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { Tabs } from "./tabs";
 import { FolderStack } from "./stacks/FolderStack";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { selectNotes, fetchAllNotes } from "../features/notesSlice";
+import { useEffect } from "react";
 export const MainNavigation = () => {
+
+  const dispatch = useDispatch();
+  // const notes = useSelector(selectNotes);
+  const notes = useSelector((state) => state.notes.notes);
+
+  useEffect(() => {
+    dispatch(fetchAllNotes());
+  }, [dispatch]);
+
+
+  console.log("notes :", notes);
+
+
   return (
+
     <NavigationContainer>
       <FolderStack />
       {/* <Tabs /> */}
