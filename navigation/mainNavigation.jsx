3cbe4e3eb6a9bacf19009bdfa7ffc32c263fcd86
javiscripto <javier.mecker94@gmail.com>
2025-01-12 10:00:38 -0300

    borro console.logs

--- navigation/mainNavigation.jsx
+++ navigation/mainNavigation.jsx
@@ -2,21 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Tabs } from "./tabs";
 import { FolderStack } from "./stacks/FolderStack";
 import { useDispatch, useSelector } from 'react-redux';
-import { selectNotes, fetchAllNotes } from "../features/notesSlice";
+import { selectNotes, fetchAllNotes, fetchFolders } from "../features/notesSlice";
 import { useEffect } from "react";
 export const MainNavigation = () => {
 
   const dispatch = useDispatch();
-  // const notes = useSelector(selectNotes);
-  const notes = useSelector((state) => state.notes.notes);
 
   useEffect(() => {
     dispatch(fetchAllNotes());
+    dispatch(fetchFolders());
   }, [dispatch]);
-
-
-  console.log("notes :", notes);
-
 
   return (
 
