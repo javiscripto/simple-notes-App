30a70eed1a15c9876338a5d2c243255cc2805b04
javiscripto <javier.mecker94@gmail.com>
2025-01-12 17:08:37 -0300

    implemento useFocusEffect para actualizar estado global en folders y modifico la funcion de eliminar carpeta

--- screens/home.jsx
+++ screens/home.jsx
@@ -1,20 +1,26 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { CustomButton } from "../components/customButton";
-import { useNavigation } from "@react-navigation/native";
-import { getAllFolders } from "../DB/index";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteFolderAsync } from "../features/notesSlice";
+import { deleteFolderAsync, fetchAllNotes, fetchFolders } from "../features/notesSlice";
 
 export default function Folder() {
   const [optionVisible, setOptionsVisible] = useState(false);
   const { navigate } = useNavigation();
   const dispatch = useDispatch();
 
-  const folders = useSelector((state) => state.notes.folders);
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(fetchFolders());
+      dispatch(fetchAllNotes());
 
+      return () => { };
+    }, [])
+  )
+  const folders = useSelector((state) => state.notes.folders);
   const handleFolderNotesPress = (id, name) => {
     navigate("Notes", { id, name })
   }
@@ -27,22 +33,21 @@ export default function Folder() {
     setOptionsVisible(false)
   }
   const deleteFolderPress = (id, name) => {
-    deleteFolder(id);
-    // Alert.alert(
-    //   "Eliminar Carpeta",
-    //   `Estas seguro que quieres eliminar la carpeta "${name}"?`,
-    //   [
-    //     {
-    //       text: "Cancelar",
-    //       onPress: () => setOptionsVisible(false),
-    //       style: "cancel",
-    //     },
-    //     {
-    //       text: "Eliminar",
-    //       onPress: () => deleteFolder(id),
-    //     },
-    //   ]
-    // )
+    Alert.alert(
+      "Eliminar Carpeta",
+      `Estas seguro que quieres eliminar la carpeta "${name}" y todo su contenido?`,
+      [
+        {
+          text: "Cancelar",
+          onPress: () => setOptionsVisible(false),
+          style: "cancel",
+        },
+        {
+          text: "Eliminar",
+          onPress: () => deleteFolder(id),
+        },
+      ]
+    )
   }
 
   const renderItem = ({ item }) => (
@@ -51,6 +56,7 @@ export default function Folder() {
       onPress={() => handleFolderNotesPress(item.id, item.name)}>
       <Ionicons name="folder" size={80} color={"#f90000"} />
       <Text style={style.text}>{item.name}</Text>
+      <Text style={style.description}>{item.description}</Text>
       {
         optionVisible && (
           <TouchableOpacity onPress={() => deleteFolderPress(item.id, item.name)}>
@@ -65,17 +71,15 @@ export default function Folder() {
     <SafeAreaView style={style.container}>
       <Text style={style.title}>Carpetas</Text>
       <View style={style.content}>
-        {folders.length === 0 ? (<Text>lista de carpetas vacia</Text>) : (
-          <FlatList
-            contentContainerStyle={style.flatList}
-            initialNumToRender={8}
-            data={folders}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id.toString()}
-            numColumns={2}
-          />
-        )
-        }
+        <FlatList
+          contentContainerStyle={style.flatList}
+          initialNumToRender={8}
+          data={folders}
+          ListEmptyComponent={<Text>Aun no hay carpetas</Text>}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={2}
+        />
       </View>
 
       <CustomButton onPress={() => navigate("CreateFolder")}>
@@ -111,6 +115,12 @@ const style = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
     marginTop: 8,
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 12,
+    color: "#666",
+    marginTop: 4,
     textAlign: "center",
   },
 });
